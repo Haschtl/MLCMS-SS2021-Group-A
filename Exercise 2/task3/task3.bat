10f7aa93572d2b77530643d3ac0c94fbc2b9b416
Sebastian Keller <sebastian.keller@protonmail.com>
2021-05-10 16:06:07 +0200

    added bash-script for task3

--- Exercise 2/task3/task3.bat
+++ Exercise 2/task3/task3.bat
@@ -1,2 +1,3 @@
 cd ..
 java -jar vadere-console.jar scenario-run --scenario-file "./scenarios/RiMEA 6 OSM.scenario" --output-dir="./out"
+java -jar vadere-console.jar scenario-run --scenario-file "./scenarios/RiMEA 6 OSM_task3.scenario" --output-dir="./out"
