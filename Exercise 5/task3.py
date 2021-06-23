98459b4859d31f5b5a33e706962193f208e397b6
umutyazgan <umut.yaz@gmail.com>
2021-06-24 00:47:31 +0300

    Add Linear solutions in task 3

--- Exercise 5/task3.py
+++ Exercise 5/task3.py
@@ -10,9 +10,35 @@ import task1
 import task2
 from helper import InteractiveMode, show_and_save, load_data, sort
 
-def linear_vf(x0, x1, dt, A):
+
+def scatter_data(x, y, name="", labels=["x", "y"], title=""):
+    """
+    Scatter and save 2-D data.
+
+    Parameters
+    ----------
+    x : (N, 1) array-like
+        x coordinates of the data.
+    y : (N, 1) array-like
+        y coordinates of the data.
+    name : string, optional
+        Filename addition. Default: ""
+    labels : list, optional
+        Labels of axes. Default: ["x", "y"]
+    title : string, optional
+        Title of the plot. Default: ""
+    """
+    plt.figure()
+    plt.scatter(x, y, label=title)
+    plt.title(title)
+    plt.legend()
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    show_and_save("task3_{}".format(name))
+
+
+def linear_vf(x0, x1, dt, A, t_end=0.1):
     # Solve x_dot=A*x with all x_0^(k) as initial points until
-    t_end = 0.1
     x = task2.create_trajectory(x0,A,dt,t_end)
     # The resulting points are estimates for x_1^(k).
 
@@ -42,6 +68,7 @@ def nonlinear_vf(x0, x1, yi):
     print("Mean squared error: {}".format(mse))
 
 if __name__ == "__main__":
+    # NOTE: This file is a mess mostly because of last day solution trials.
     nonlinear_vectorfield_x0 = load_data("nonlinear_vectorfield_data_x0.txt")
     nonlinear_vectorfield_x1 = load_data("nonlinear_vectorfield_data_x1.txt")
 
@@ -53,10 +80,26 @@ if __name__ == "__main__":
     print(A)
     linear_vf(nonlinear_vectorfield_x0, nonlinear_vectorfield_x1, dt, A)
     
+    diff_vector = nonlinear_vectorfield_x1 - nonlinear_vectorfield_x0
+    # rbfi = Rbf(diff_vector[:,0], diff_vector[:,1], function='gaussian', epsilon=0.00001)
+    rbfi = Rbf(nonlinear_vectorfield_x0[:,0], nonlinear_vectorfield_x0[:,1], function='gaussian', epsilon=0.00001)
+    # yi = rbfi(nonlinear_vectorfield_x0)
+    yi = rbfi(nonlinear_vectorfield_x1[:,0])
+    # print(yi.shape)
+    # mse = ((yi - nonlinear_vectorfield_x1)**2).mean(axis=0)
+    # print(mse)
 
-    yi, error = task1.approximate_nonlinear_function(nonlinear_vectorfield_x0[:,0], nonlinear_vectorfield_x0[:,1], nonlinear_vectorfield_x1[:,0], epsilon=5)
+    # yi, error = task1.approximate_nonlinear_function(nonlinear_vectorfield_x0[:,0], nonlinear_vectorfield_x0[:,1], nonlinear_vectorfield_x1[:,0], epsilon=0.00001)
     #mse = ((yi - nonlinear_vectorfield_x1[:,1])**2).mean()
     # mse = ((yi - nonlinear_vectorfield_x1[:,1])**2).mean(axis=0)
     # print(mse)
 
     nonlinear_vf(nonlinear_vectorfield_x0, nonlinear_vectorfield_x1, yi)
+    # nonlinear_vf(nonlinear_vectorfield_x0, yi, yi[:,1])
+    
+    # Solve for a larger time using linear method.
+    solutions = []
+    for i in range(10):
+        solution = task2.create_trajectory(nonlinear_vectorfield_x0,A,dt,i)
+        scatter_data(solution[:,0], solution[:,1], name="solution{}".format(i), title="Solution for t={}".format(i))
+    
